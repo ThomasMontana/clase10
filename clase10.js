d4bc8cb366358064a9782947371599a7421bec30
ThomasMontana <tom_tain@hotmail.com>
2021-10-24 20:51:41 -0300

    actualizacion

---
+++ clase10.js
@@ -0,0 +1,15 @@
+const cambioDeColor = () => {
+    $("#texto").css("background-color", "blue");
+}
+
+const MostrarOcultar = () => {
+    $("#texto").toggle(500);
+}
+
+const Texto = () => {
+    $("#text").html("hola mundo");
+}
+
+let Colores = [,"Rojo","Amarillo","Verde","Violeta","Rosa","Celeste","",""];
+
+const 
